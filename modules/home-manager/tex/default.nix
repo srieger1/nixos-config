aa16348f57b86b6e568b6669389dcdac0853c112
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2026-07-31 21:32:09 +0200

    update

--- modules/home-manager/tex/default.nix
+++ modules/home-manager/tex/default.nix
@@ -1,12 +1,13 @@
 { config, pkgs, ... }:
-texliveSmall.withPackages {
-  scheme-basic
-  dvisvgm dvipng # for preview and export as html
-  wrapfig amsmath ulem hyperref capt-of # default from example in nixos wiki
-  wasy biblatex adjustbox arydshln lstaddons pict2e diagbox wasysym babel babel-german german beamer sttools caption times metafont csquotes float cite amsfonts algorithms algorithmicx graphics listings xcolor enumitem multirow subfig bytefield chngcntr siunitx cleveref
-  xurl # for p4 intro acn
-  parskip eurosym comment titlesec mathtools xifthen mdframed ifmtarg zref needspace helvetic microtype pgfgantt fontawesome5 # dfg sachbeihilfe, sach par local ai
-  subfiles setspace soul;
-  #(setq org-latex-compiler "lualatex")
-  #(setq org-preview-latex-default-process 'dvisvgm)
-};
+texliveSmall.withPackages (ps: with ps; [
+    scheme-basic
+    dvisvgm dvipng # for preview and export as html
+    wrapfig amsmath ulem hyperref capt-of # default from example in nixos wiki
+    wasy biblatex adjustbox arydshln lstaddons pict2e diagbox wasysym babel babel-german german beamer sttools caption times metafont csquotes float cite amsfonts algorithms algorithmicx graphics listings xcolor enumitem multirow subfig bytefield chngcntr siunitx cleveref
+    xurl # for p4 intro acn
+    parskip eurosym comment titlesec mathtools xifthen mdframed ifmtarg zref needspace helvetic microtype pgfgantt fontawesome5 # dfg sachbeihilfe, sach par local ai
+    subfiles setspace soul;
+    #(setq org-latex-compiler "lualatex")
+    #(setq org-preview-latex-default-process 'dvisvgm)
+  ]
+);
