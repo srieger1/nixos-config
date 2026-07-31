868115ae00a81122fac7eaacd146bf720586c509
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2026-07-31 21:32:42 +0200

    update

--- modules/home-manager/tex/default.nix
+++ modules/home-manager/tex/default.nix
@@ -6,7 +6,7 @@ texliveSmall.withPackages (ps: with ps; [
     wasy biblatex adjustbox arydshln lstaddons pict2e diagbox wasysym babel babel-german german beamer sttools caption times metafont csquotes float cite amsfonts algorithms algorithmicx graphics listings xcolor enumitem multirow subfig bytefield chngcntr siunitx cleveref
     xurl # for p4 intro acn
     parskip eurosym comment titlesec mathtools xifthen mdframed ifmtarg zref needspace helvetic microtype pgfgantt fontawesome5 # dfg sachbeihilfe, sach par local ai
-    subfiles setspace soul;
+    subfiles setspace soul
     #(setq org-latex-compiler "lualatex")
     #(setq org-preview-latex-default-process 'dvisvgm)
   ]
