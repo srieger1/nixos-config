3d312fb0319cb4705e6218bf4f697f69b7189715
Sebastian Rieger <sebastian.rieger@informatik.hs-fulda.de>
2026-08-13 09:58:13 +0200

    update

--- flake.nix
+++ flake.nix
@@ -70,7 +70,7 @@
         let
           private =
             if builtins.pathExists ./hosts/giedi-prime/private.nix
-            then import ./hosts/giedi-prime/private.nix
+            then import ./hosts/giedi-prime/private.nix;
             #else import ./hosts/giedi-prime/private.nix.example;
         in nixpkgs.lib.nixosSystem {
           inherit system;
